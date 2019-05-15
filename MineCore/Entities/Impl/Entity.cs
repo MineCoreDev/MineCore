abe91de05cfe5b301a27c7bf9149d2ad87c04dd9
hiroki19990625 <hiroki19990625@gmail.com>
2019-05-16 01:44:08 +0900

    ダイアグラムを追加

--- MineCore/Entities/Impl/Entity.cs
+++ MineCore/Entities/Impl/Entity.cs
@@ -1,12 +1,15 @@
-﻿namespace MineCore.Entities.Impl
+﻿using System;
+
+namespace MineCore.Entities.Impl
 {
     public class Entity : IEntity
     {
         public long EntityId { get; }
         public long EntityRuntimeId { get; }
+
         public void Close()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
